af2d9749ec2bd0314d257f09913aee2d60d59994
Victor <matt_shadows08@hotmail.com>
2025-01-28 14:28:47 +0100

    Add Router

---
+++ pruebaLisData/src/Routes/Router.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
--- pruebaLisData/src/main.tsx
+++ pruebaLisData/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
-import App from "./App.tsx";
+import Router from "./Routes/Router.tsx";
 import { ThemeProvider } from "./context/Theme/ThemeContext.tsx";
 
 const queryClient = new QueryClient();
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <App />
+        <Router />
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
