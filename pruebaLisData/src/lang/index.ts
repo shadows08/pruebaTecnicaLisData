d53088643f0ee871660d088c6eeb2f18680e594b
Victor <matt_shadows08@hotmail.com>
2025-01-28 13:25:27 +0100

    add i18n

---
+++ pruebaLisData/src/lang/index.ts
@@ -0,0 +1,5 @@
+import LocalizedStrings from 'react-localization';
+import { enEN } from './en/enEN';
+import { esES } from './es/esES';
+
+export const i18n = new LocalizedStrings({ es: esES, en: enEN });
