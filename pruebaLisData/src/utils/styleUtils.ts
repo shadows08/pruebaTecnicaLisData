a1b3f2cc8d694c7a34286641e411a7c00adbc255
Victor <matt_shadows08@hotmail.com>
2025-01-30 09:47:02 +0100

    add Palette options

--- pruebaLisData/src/utils/styleUtils.ts
+++ pruebaLisData/src/utils/styleUtils.ts
@@ -4,4 +4,49 @@ export const DEFAULT_THEME = createTheme({
   typography: {
     fontFamily: "'Bebas Neue', sans-serif",
   },
+  palette: {
+    header: {
+      background: "#FFFFFF",
+    },
+    blackLayout: {
+      background: "#1B1D29",
+    },
+    colors: {
+      white: "#FFFFFF",
+    },
+  },
 });
+
+import "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    header: {
+      background: string;
+      text: string;
+    };
+    blackLayout: {
+      background: string;
+    };
+    colors: {
+      white: string;
+      black: string;
+      gray: string;
+    };
+  }
+
+  interface PaletteOptions {
+    header?: {
+      background?: string;
+      text?: string;
+    };
+    blackLayout?: {
+      background?: string;
+    };
+    colors?: {
+      white?: string;
+      black?: string;
+      gray?: string;
+    };
+  }
+}
