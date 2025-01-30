2c091f79f917704b81f108d68fe0ecd62663114e
Victor <matt_shadows08@hotmail.com>
2025-01-30 10:45:29 +0100

    add product DTO

---
+++ pruebaLisData/src/api/types/model/dtos/productDTO.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  stock_quantity: number;
+  average_rating: number;
+  is_free_shipping: boolean;
+  id_category: number;
+  id_subcategory: number;
+  id_color: number;
+}
